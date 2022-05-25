8999f84865f2082f825130d7345d27b0ea1adf12
Robson Leal <devleal@hotmail.com>
2022-05-25 11:15:50 -0300

    Construndo ropape, refatorando e colocando animações

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function scrolling() {
+    showNavOnScroll();
+    showBackToTopOnScroll();
+}
+
+function showNavOnScroll() {
     if (scrollY) navigation.classList.add('scroll');
     else navigation.classList.remove('scroll');
+}
+
+function showBackToTopOnScroll() {
+    if (scrollY > 400) backToTop.classList.add('show');
+    else backToTop.classList.remove('show');
 }
 
 function openMenu() {
@@ -10,3 +20,26 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700
+}).reveal(`
+    #home .subtitle,
+    #home .title,
+    .home__content,
+    .home__stats,
+    .services__subtitle,
+    .services__title,
+    .service,
+    .about__subtitle,
+    .about__title,
+    .about .text-generic,
+    .about__img,
+    .contact__title,
+    .contact__item,
+    #contact .button,
+    .contact__img,
+    .footer
+    `);
